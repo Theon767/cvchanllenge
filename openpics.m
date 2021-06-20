1a691c4c430716303f3b91436f00c2f4d1e97c8c
Theon767 <52738560+Theon767@users.noreply.github.com>
2021-06-20 17:22:33 +0800

    Delete openpics.m

--- openpics.m
+++
@@ -1,8 +0,0 @@
-function pics=openpics(file_path)
-jpgs = dir(strcat(file_path,'*.jpg'));
-pics={};   %%refer to all the pictures
-for i=1:length(jpgs)
-pics{i}=imread(strcat(file_path,jpgs(i).name));
-pics{i}=rgb2gray(pics{i});
-end
-end
