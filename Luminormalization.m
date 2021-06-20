b377ed267597fac36699a4843c97f8bbc0c82100
Theon767 <52738560+Theon767@users.noreply.github.com>
2021-06-20 17:21:35 +0800

    Delete Luminormalization.m

--- Luminormalization.m
+++
@@ -1,8 +0,0 @@
-function normalized=Luminormalization(pics,contrast,lumi)
-normalized={};
-[m,n]=size(pics{1});
-for i=1:length(pics)
-    A=double(pics{i});
-    A=(A-(sum(sum(A))/m/n).*ones(m,n))./std2(A);
-    normalized{i}=uint8(round(A*contrast+lumi));
-end
